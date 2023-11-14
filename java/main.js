decdbce8d3181b692d0d4a63bb23d6b70b98a11b
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-14 01:36:57 +0100

    select picture

--- java/main.js
+++ java/main.js
@@ -1,6 +1,11 @@
+
+
+
+
 /* scrivo le mie variabili */
 const buttonLeft=document.getElementById("btn-left");
 const buttonRight=document.getElementById("btn-right");
+let galleries=[];
 const images = [
     {
         image: "./img 4/01.webp",
@@ -29,70 +34,158 @@ let container=document.getElementById("carousel")
 
 
 /* per ognuno degli elementi presenti nelle images,sposto i dati nell'HTML */
+/* voglio aggiungere le card nell'html */
 let i=0;
 while ( i < images.length) {
     let immagine = images[i];
-    container.innerHTML+= `
+    container.innerHTML+= 
+    `
     <div class="card">
     <div class="foto-wrapper">
     <img src="${images[i].image}" class="foto">
     </div>
     <div class="text-foto">${images[i].title}</div>
     <p class="text-foto">${images[i].text}</p>
-    </div>`
-    i++;
+    </div>
+    `
     
+    /* aumentanto l'indice dell'arrey in cui la card risiede */
+    i++;
 }    
-/* ******************* */
 
-/* voglio svotare l'html */
-
-
-
-/* arrow function */
-/* let prova=() =>console.log("funzione eseguita");
-*/
-/* ()=>"funzione..."*/
-/* arrow function prende scoop dal padre */
-
-let foto0 =0;
-let immagini= document.getElementsByClassName("card");
-
-for (let i = 1; i < immagini.length; i++) {
-    immagini[i].classList.add("invisible");
-
-}
-
-let finalfoto=immagini.length -1;
+/* invoco la funzione PULSANTI */
 pulsanti();
 
 
+/* funzione che fa funzionare il pulsante di sinistra e di destra */
 function pulsanti(){
+
+    /* creo una variabile che possa crescere e indicare il valore della posizione dell'arrey */
+    let foto0 =0;
+    /* creo un arrey di cards,chiamando tutti gli alementi della stessa classe */
+    let immagini= document.getElementsByClassName("card");
+    /* rendo tutti invisibili tranne il primo (i=0)*/
+    for (let i = 1; i < immagini.length; i++) {
+        immagini[i].classList.add("invisible");
+    }
+    /* dichiaro il limite,per lo spostamento futuro delle cards */
+    let finalfoto=immagini.length -1;
     
-    
+
+
+
+    /* pulsante destro */
     buttonRight.addEventListener("click",function(){
+        /* se la foto corrente(indicata dall'indice "foto0") non è l'ultima.. */
         if(foto0 < finalfoto){
+            /* nascondi la foto corrente */
             immagini[foto0].classList.add("invisible");
+            /* seleziona la foto seguente */
             foto0++
+            /* mostrala */
             immagini[foto0].classList.remove("invisible");
         }
+        /* altrimenti se la foto corrente è l'ultima */
         else{
+            /* nascondi la foto corrente  */
             immagini[foto0].classList.add("invisible");
+            /* riparti da capo */
             foto0 = 0;
+            /* rendi visibile la foto iniziale */
             immagini[foto0].classList.remove("invisible");
         }
       
         
     })
-    
+    /* pulsante sinistro */
     buttonLeft.addEventListener("click", function () {
-        if(foto0 > 0){
+        if(foto0 > 0){    
+            /* nascondi la foto corrente */
             immagini[foto0].classList.add("invisible");
-            foto0++
+            /* seleziona la foto seguente */
+            foto0--
+            /* mostrala */
+            immagini[foto0].classList.remove("invisible");
+        }
+        else {
+            /* nascondi la foto corrente  */
+            immagini[foto0].classList.add("invisible");
+            /* riparti da capo */
+            foto0 = finalfoto ;
+            /* rendi visibile la foto iniziale */
             immagini[foto0].classList.remove("invisible");
         }
     });
 }
 
 
+let gallery=document.getElementById("gallery");
+/* creo una galleria di card laterale */
+for (let i=0; i < images.length;i++){
+    let immagine = images[i];
+    gallery.innerHTML+= `
+    <div class="foto-gallery"><img src="${images[i].image}" class="foto" alt="foto"></div>
+`
+    galleries.push(gallery.innerHTML);
+}    
+/* ***************** */
+let sideCard=document.getElementsByClassName("foto-gallery")
 
+sideCard[0].addEventListener("click",function () {
+     container.innerHTML= `
+     <div class="card">
+     <div class="foto-wrapper">
+     <img src="${images[0].image}" class="foto">
+     </div>
+     <div class="text-foto">${images[0].title}</div>
+     <p class="text-foto">${images[0].text}</p>
+     </div>
+     `
+})
+sideCard[1].addEventListener("click",function () {
+    container.innerHTML= `
+     <div class="card">
+     <div class="foto-wrapper">
+     <img src="${images[1].image}" class="foto">
+     </div>
+     <div class="text-foto">${images[1].title}</div>
+     <p class="text-foto">${images[1].text}</p>
+     </div>
+     `
+})
+sideCard[2].addEventListener("click",function () {
+    container.innerHTML= `
+     <div class="card">
+     <div class="foto-wrapper">
+     <img src="${images[2].image}" class="foto">
+     </div>
+     <div class="text-foto">${images[2].title}</div>
+     <p class="text-foto">${images[2].text}</p>
+     </div>
+     `
+    console.log(this);
+})
+sideCard[3].addEventListener("click",function () {
+    container.innerHTML= `
+     <div class="card">
+     <div class="foto-wrapper">
+     <img src="${images[3].image}" class="foto">
+     </div>
+     <div class="text-foto">${images[3].title}</div>
+     <p class="text-foto">${images[3].text}</p>
+     </div>
+     `
+    console.log(this);
+})
+sideCard[4].addEventListener("click",function () {
+    container.innerHTML= `
+     <div class="card">
+     <div class="foto-wrapper">
+     <img src="${images[4].image}" class="foto">
+     </div>
+     <div class="text-foto">${images[4].title}</div>
+     <p class="text-foto">${images[4].text}</p>
+     </div>
+     `
+    console.log(this);
+})
